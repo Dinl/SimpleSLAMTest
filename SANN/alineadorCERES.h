ef97fd19abb605a018a52727224a311f2f00c284
2gomex@gmail.com <2gomex@gmail.com@ed41555c-670e-476c-b7cc-625a6f8e5eb2>
2015-02-20 04:48:17 +0000

    Primera aproximacion con calculo de ceres

--- SANN/alineadorCERES.h
+++ SANN/alineadorCERES.h
@@ -13,45 +13,56 @@
 using namespace ceres;
 
 
-class mifunciondecosto{
+class alineadorM9{
 public:
 	
-	//Constructor
-	mifunciondecosto(double k): k_(k){};
+	//Constructor, el punto de la primera imagen 
+	alineadorM9(double observed_x, double observed_y){
+		P1_x = observed_x;
+		P1_y = observed_y;
+	};
 
 	//Operador de costo
 	template <typename T>
-	bool operator()(const T* const x, const T* const y, T* e) const {
-		e[0] = T(k_) - x[0] * y[0] - x[1] * y[1];
+	bool operator()(const T* const Tmatrix, const T* const punto, T* residuos) const {
+		
+		//Computar la rotacion
+		T p[3];
+		ceres::AngleAxisRotatePoint(Tmatrix, punto, p);
+
+		//Computar la traslacion
+		p[0] += Tmatrix[3]; 
+		p[1] += Tmatrix[4]; 
+		p[2] += Tmatrix[5];
+
+		//Computar los centros de distorcion
+		T xp = - p[0] / p[2];
+		T yp = - p[1] / p[2];
+
+		//Aplicar la distorsion radial
+		const T& l1 = Tmatrix[7];
+		const T& l2 = Tmatrix[8];
+		T r2 = xp*xp + yp*yp;
+		T distortion = T(1.0) + r2  * (l1 + l2  * r2);
+
+		//Predecir la posicion final
+		const T& focal = Tmatrix[6];
+		T predicted_x = focal * distortion * xp;
+		T predicted_y = focal * distortion * yp;
+
+		// The error is the difference between the predicted and observed position.
+		residuos[0] = predicted_x - T(P1_x);
+		residuos[1] = predicted_y - T(P1_y);
+
 		return true;
 	}
 
+	static ceres::CostFunction* Create(const double observed_x, const double observed_y) {
+		return (new ceres::AutoDiffCostFunction<alineadorM9, 2, 9, 3>(new alineadorM9(observed_x, observed_y)));
+	}
+
 private:
-	double k_;
+	double P1_x, P1_y;
 };
-
-int main(int argc, char** argv) {
-
-	double x[2];
-	double y[2];
-
-	x[0] = 0.0;
-	x[1] = 1.0;
-	y[0] = 0.0;
-	y[1] = 2.0;
-
-	Problem problem;
-	CostFunction* funcioncosto = new AutoDiffCostFunction<mifunciondecosto,1,2,2>(new mifunciondecosto(1.0));
-	problem.AddResidualBlock(funcioncosto, NULL, x, y);
-
-
-	ceres::Solver::Options options;
-	options.linear_solver_type = ceres::DENSE_SCHUR;
-	options.minimizer_progress_to_stdout = true;
-	ceres::Solver::Summary summary;
-	ceres::Solve(options, &problem, &summary);
-	std::cout << summary.FullReport() << "\n";
-	return 0;
-}
 
 //#endif
