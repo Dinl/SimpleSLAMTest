0fa1105543838d8cb077de1c816179e673602b63
2gomex@gmail.com <2gomex@gmail.com@ed41555c-670e-476c-b7cc-625a6f8e5eb2>
2015-03-11 05:07:02 +0000

    Comentarios y division del metodo 2D/3D

--- trunk/SANN/alineadorCERES.h
+++ trunk/SANN/alineadorCERES.h
@@ -14,41 +14,37 @@
 using namespace ceres;
 
 
-class alineadorM9{
+
+/***********************************************************************************
+	*	Clase T para realizar la alineacion de observaciones entre 2 imagenes que se
+	*	proyectan al espacio.
+************************************************************************************/
+class alineador2D{
 public:
-	
-	//Constructor, el punto de la primera imagen 
-	alineadorM9(double original_x, double original_y, double original_z){
+	/***********************************************************************************
+	*	Constructor:
+	*	Se crea con el punto XY de la primera imagen 
+	***********************************************************************************/
+	alineador2D(double original_x, double original_y){
 		P1_x = original_x;
 		P1_y = original_y;
-		P1_z = original_z;
 	};
 
 	/***********************************************************************************
-	*	Operador de costo utilizado por CERES
-	*	Tmatrix -> Ingresa como un array de 6 valores:
-	*				Tmatrix[0] -> Angulo yaw
-	*				Tmatrix[1] -> Angulo pitch
-	*				Tmatrix[2] -> Angulo roll
-	*				Tmatrix[3] -> Traslacion en X
-	*				Tmatrix[4] -> Traslacion en Y
-	*				Tmatrix[5] -> Traslacion en Z
-	*				Tmatrix[6] -> distancia focal X
-	*				Tmatrix[7] -> distancia focal Y
-	*				Tmatrix[8] -> Centro de la imagen en X
-	*				Tmatrix[9] -> Centro de la imagen en Y
-	*				Tmatrix[10] -> K1
-	*				Tmatrix[11] -> K2
+	*	Operador
+	*	Es el operador quien recibe y calcula los residuos para cada punto
 	*
-	*	punto -> Ingresa como un array de 3 valores con XYZ del destino
-	*				punto[0] -> X
-	*				punto[1] -> Y
-	*				punto[2] -> Z
+	*	Textrinseca -> Array 6 double: [Rx Ry Rz X Y Z] rotaciones en el sentido de euler
+	*	Tintrinseca -> Array 6 double: [Fx Fy Cx Cy K1 K2] parametros de calibacion
+	*	punto2D		-> Punto XY en la imagen de destino
+	*	punto3D		-> Punto XYZ correspondiente al punto2D
 	*
-	*	residuos -> Array de 3 valores con los residuos en XYZ usado por ceres
-	*
-	************************************************************************************/
-
+	*	El proceso consiste en:
+	*	
+	*	punto2D -> (intrinseca) -> punto2d3d -> (extrinseca) -> transformedpoint3d
+	*	-> (intrinseca) -> transformedPunto2d
+	*	residuo = original - transformedPunto2d
+	***********************************************************************************/
 	template <typename T>
 	bool operator()(const T* const Textrinseca, const T* const Tintrinseca, const T* const punto2D, const T* const punto3D, T* residuos) const {
 		
@@ -91,6 +87,62 @@ public:
 		T predicted_y = distortion * yp;
 
 		//Obtener la distancia residual
+		residuos[0] = T(P1_x) - predicted_x;
+		residuos[1] = T(P1_y) - predicted_y;
+
+		return true;
+	}
+
+	static ceres::CostFunction* Create(const double observed_x, const double observed_y) {
+		return (new ceres::AutoDiffCostFunction<alineador2D, 2, 6, 6, 2, 3>(new alineador2D(observed_x, observed_y)));
+	}
+
+private:
+	double P1_x, P1_y;
+};
+
+/***********************************************************************************
+	*	Clase T para realizar la alineacion de observaciones entre 2 nubes
+	*
+************************************************************************************/
+class alineador3D{
+public:
+	
+	/***********************************************************************************
+	*	Constructor:
+	*	Se crea con el punto XYZ de la primera nube 
+	***********************************************************************************/
+	alineador3D(double original_x, double original_y, double original_z){
+		P1_x = original_x;
+		P1_y = original_y;
+		P1_z = original_z;
+	};
+
+	/***********************************************************************************
+	*	Operador
+	*	Es el operador quien recibe y calcula los residuos para cada punto
+	*
+	*	Textrinseca -> Array 6 double: [Rx Ry Rz X Y Z] rotaciones en el sentido de euler
+	*	punto3D		-> Punto XYZ correspondiente al punto2D
+	*
+	*	El proceso consiste en:
+	*	
+	*	punto2D -> (intrinseca) -> punto2d3d -> (extrinseca) -> transformedpoint3d
+	*	-> (intrinseca) -> transformedPunto2d
+	*	residuo = original - transformedPunto2d
+	***********************************************************************************/
+	template <typename T>
+	bool operator()(const T* const Textrinseca, const T* const punto3D, T* residuos) const {
+		
+		//Obtener el punto transformado en rotacion y traslacion
+		T transformedPoint3D[3];
+		ceres::AngleAxisRotatePoint(Textrinseca, punto3D, transformedPoint3D);
+
+		transformedPoint3D[0] += Textrinseca[3];
+		transformedPoint3D[1] += Textrinseca[4];
+		transformedPoint3D[2] += Textrinseca[5];
+
+		//Obtener la distancia residual
 		residuos[0] = T(P1_x) - transformedPoint3D[0];
 		residuos[1] = T(P1_y) - transformedPoint3D[1];
 		residuos[2] = T(P1_z) - transformedPoint3D[2];
@@ -99,7 +151,7 @@ public:
 	}
 
 	static ceres::CostFunction* Create(const double observed_x, const double observed_y, const double observed_z) {
-		return (new ceres::AutoDiffCostFunction<alineadorM9, 3, 6, 6, 2, 3>(new alineadorM9(observed_x, observed_y, observed_z)));
+		return (new ceres::AutoDiffCostFunction<alineador3D, 3, 6, 3>(new alineador3D(observed_x, observed_y, observed_z)));
 	}
 
 private:
